5d27b7076e04fe53fc09ddc508120ed763d7e480
RaynaRaven <carolmarjara@gmail.com>
2023-08-09 19:33:49 +0100

    fix case typo causing deploy build error

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -3,7 +3,7 @@ import PageTemplate from "../components/templateMovieListPage";
 import { getMovies } from "../api/tmdb-api";
 import { useQuery } from "react-query";
 import Spinner from "../components/spinner";
-import AddToFavouritesIcon from "../components/cardIcons/addtoFavourites";
+import AddToFavouritesIcon from "../components/cardIcons/addToFavourites";
 import { TypeContext } from "../contexts/typeContext";
 
 const HomePage = () => {
