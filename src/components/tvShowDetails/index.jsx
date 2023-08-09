678f1e12af56d3a93c4ce225f2beb43b771f4187
RaynaRaven <carolmarjara@gmail.com>
2023-08-09 19:57:38 +0100

    remove unused import statement

--- src/components/tvShowDetails/index.jsx
+++ src/components/tvShowDetails/index.jsx
@@ -8,7 +8,6 @@ import Typography from "@mui/material/Typography";
 import NavigationIcon from "@mui/icons-material/Navigation";
 import Fab from "@mui/material/Fab";
 import Drawer from "@mui/material/Drawer";
-// import MovieReviews from '../movieReviews';
 
 const styles = {
   chipSet: {
--- src/pages/searchMoviesPage.jsx
+++ src/pages/searchMoviesPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import MovieSearchForm from '../components/movieSearchForm';
 import TemplatePage from '../components/templateMovieListPage';
-import MovieList from '../components/MovieList';
 
 const SearchMoviePage = () => {
     const [movies, setMovies] = useState([]);
